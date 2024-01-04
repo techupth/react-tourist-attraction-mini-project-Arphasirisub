f2f33d77f827343f32d16cbb7f9a520f8f93d800
Am <arphasirisubb@gmail.com>
2024-01-04 13:12:16 +0700

    tourist page website

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import TouristList from "./component/TouristList";
 
 function App() {
-  return <div className="App">{/* Start coding here */}</div>;
+  return (
+    <div className="App">
+      <TouristList />
+    </div>
+  );
 }
 
 export default App;
